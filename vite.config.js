ae353878c4f6416211ba091e66545789f799b053
somar <somar@163.com>
2025-02-13 17:45:04 +0800

    4 commit

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,33 @@
+// import { defineConfig } from 'vite'
+// import vue from '@vitejs/plugin-vue'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [vue()],
+
+//   // 配置别名@即为src
+//   resolve:{
+//     alias:[
+//       {
+//         find:"@",
+//         replacement:"/src",        
+//       }
+//     ]
+//   }
+// })
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from 'path'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-
-  // 配置别名@即为src
-  resolve:{
-    alias:[
-      {
-        find:"@",
-        replacement:"/src",        
-      }
-    ]
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src')
+    }
+  },
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets'
   }
 })
